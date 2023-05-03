af50265688baa2b800b426a703acb92be7fd2d2a
Sandeep-svf <svf.sandeep@gmail.com>
2023-05-03 17:27:00 +0530

    sam

--- app/src/main/java/com/shamsaha/victim/adapter/ResourcesPerCountryAdapter.java
+++ app/src/main/java/com/shamsaha/victim/adapter/ResourcesPerCountryAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 import com.shamsaha.R;
 
@@ -26,6 +27,13 @@ public class ResourcesPerCountryAdapter extends RecyclerView.Adapter<PerCountryV
     @Override
     public void onBindViewHolder(@NonNull PerCountryViewHolder holder, int position) {
 
+        holder.item_per_country_layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 
     @Override
@@ -35,7 +43,9 @@ public class ResourcesPerCountryAdapter extends RecyclerView.Adapter<PerCountryV
 }
 class PerCountryViewHolder extends RecyclerView.ViewHolder{
 
+    CardView item_per_country_layout;
     public PerCountryViewHolder(@NonNull View itemView) {
         super(itemView);
+        item_per_country_layout = itemView.findViewById(R.id.item_per_country_layout);
     }
 }
